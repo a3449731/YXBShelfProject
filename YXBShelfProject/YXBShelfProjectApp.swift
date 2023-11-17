b2f2513df765d4dcbb81db686943c8847f876d2b
gjdnxcc <381747713@qq.com>
2023-11-17 16:50:33 +0800

    Initial Commit

---
+++ YXBShelfProject/YXBShelfProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  YXBShelfProjectApp.swift
+//  YXBShelfProject
+//
+//  Created by 蓝鳍互娱 on 2023/11/17.
+//
+
+import SwiftUI
+
+@main
+struct YXBShelfProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
